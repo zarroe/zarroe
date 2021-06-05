0e270ba2a378b07a7b6592b02dbfce10f7fad657
Eli Forbes <eliforbes42@gmail.com>
2021-06-05 14:00:12 -0700

    v1.0

---
+++ src/app/App.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import logo from "../content/ZarroeLogoSeafoam-Transparent.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
+import { IconName } from "@fortawesome/fontawesome-svg-core";
+import { useId } from "@fluentui/react-hooks";
+
+const logoInfo = [
+  { name: "twitch", link: "https://twitch.tv/zarroe" },
+  { name: "twitter", link: "https://twitter.com/zarroe_" },
+  { name: "discord", link: "https://discord.gg/QBNDYxT75v" },
+  { name: "youtube", link: "https://www.youtube.com/channel/UCdNokzQEOaAniDTdOd0vKSw"}
+];
+
+interface AppLogoProps {
+  logo: any;
+}
+const AppLogo = (props: AppLogoProps) => {
+  return <img src={props.logo} className="App-logo" alt="logo" />;
+};
+
+interface SocialLinkProps {
+  name: string;
+  link: string;
+}
+
+const SocialLink = (props: SocialLinkProps) => {
+  const [hover, setHover] = useState(false)
+  const socialLinkId = useId(`social-link-${props.name}`);
+  return (
+    <div className={hover ? "icon-container-hover" : "icon-container"}>
+      <a
+        id={socialLinkId}
+        href={props.link}
+        className={"icon-container-link"}
+        onMouseOver={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <FontAwesomeIcon
+          size="2x"
+          color="#155148"
+          icon={["fab", props.name as IconName]}
+        />
+      </a>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <main className="app-main">
+        <AppLogo logo={logo} />
+        <div className="icon-tray">
+          {logoInfo.map((info) => {
+            return (
+              <SocialLink {...info} />
+            );
+          })}
+        </div>
+        <div>
+          <a className={"vod-link"} href={"https://www.youtube.com/channel/UCOSn25rdcgH5zRcpm9nU3JA"}>Check out the Vods</a>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab)
 
 ReactDOM.render(
   <React.StrictMode>
